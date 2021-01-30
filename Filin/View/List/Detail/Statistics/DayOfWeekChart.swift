8f5c6f008a12b076f45689150183ed3ff8d12605
Yeolyi <type101315@gmail.com>
2021-01-30 22:17:39 +0900

    💄 요일별 통계 그래프 디자인 개선

--- Filin/View/List/Detail/Statistics/DayOfWeekChart.swift
+++ Filin/View/List/Detail/Statistics/DayOfWeekChart.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DayOfWeekChart: View {
     
-    let habit: FlHabit
+    @ObservedObject var habit: FlHabit
     var trendToGraphHeight: [CGFloat] = []
     var dayOfWeekTrend: [Double] = []
     
@@ -18,90 +18,70 @@ struct DayOfWeekChart: View {
         self.dayOfWeekTrend = habit.dayOfWeekTrend
         trendToGraphHeight = dayOfWeekTrend.map {
             let realValue = CGFloat($0 * 120 / Double(habit.achievement.numberOfTimes))
-            return min(59, max(-59, realValue))
+            return min(60, max(-60, realValue))
         }
     }
     
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        VStack(spacing: 0) {
+        VStack(alignment: .leading, spacing: 0) {
             HStack {
                 Text("Trend(day of the week)")
                     .bodyText()
                 Spacer()
             }
-            Spacer()
-            ZStack {
-                HStack(alignment: .bottom, spacing: 8) {
-                    ForEach(0..<7) { index in
-                        if habit.dayOfWeek.contains(index+1) {
+            .padding(.bottom, 25)
+            HStack(alignment: .bottom, spacing: 8) {
+                ForEach(0..<7) { index in
+                    ZStack {
+                        VStack(spacing: 0) {
+                            Rectangle()
+                                .subColor()
+                                .opacity(0.1)
+                                .frame(width: 24, height: 120)
+                                .zIndex(0)
+                        }
                             VStack(spacing: 0) {
                                 Spacer()
                                 Text(String(round(dayOfWeekTrend[index]*10)/10))
-                                    .foregroundColor(
-                                        Date().dayOfTheWeek == index + 1 ?
-                                            habit.color : ThemeColor.subColor(colorScheme)
-                                    )
+                                    .subColor()
                                     .bodyText()
                                 Rectangle()
-                                    .foregroundColor(
-                                        Date().dayOfTheWeek == index + 1 ?
-                                            habit.color : ThemeColor.subColor(colorScheme)
+                                    .foregroundColor(habit.color)
+                                    .frame(
+                                        width: 24,
+                                        height: abs(trendToGraphHeight[index]) == 0 ? 3 : abs(trendToGraphHeight[index])
                                     )
-                                    .frame(width: 24, height: abs(trendToGraphHeight[index]))
-                                    .padding(.bottom, 3)
-                                Text(Date.dayOfTheWeekShortStr(index + 1))
-                                    .foregroundColor(
-                                        Date().dayOfTheWeek == index + 1 ?
-                                            habit.color : ThemeColor.subColor(colorScheme)
-                                    )
-                                    .bodyText()
                             }
                             .animation(.default)
                             .frame(width: 40)
-                            .offset(y: trendToGraphHeight[index] > 0 ? -60 : -60 - trendToGraphHeight[index])
-                        } else {
-                            VStack(spacing: 0) {
-                                Spacer()
-                                Text("0.0")
-                                    .subColor()
-                                    .bodyText()
-                                Rectangle()
-                                    .subColor()
-                                    .frame(width: 24, height: 60)
-                                    .padding(.bottom, 3)
-                                    .hidden()
-                                Text(Date.dayOfTheWeekShortStr(index + 1))
-                                    .subColor()
-                                    .bodyText()
-                                    .frame(width: 24)
-                            }
-                            .opacity(0.5)
-                            .frame(width: 40)
-                            .hidden()
-                        }
+                            .offset(y: trendToGraphHeight[index] == 0 ? -57 :
+                                        (trendToGraphHeight[index] > 0 ? -60 : -60 - trendToGraphHeight[index])
+                            )
+                            .zIndex(1)
+                            .opacity(habit.dayOfWeek.contains(index + 1) ? 1 : 0)
                     }
                 }
-                VStack(spacing: 0) {
-                    Spacer()
-                    Rectangle()
-                        .subColor()
-                        .frame(width: 330, height: 2)
-                    Rectangle()
-                        .foregroundColor(habit.color)
-                        .frame(width: 35, height: 60)
-                        .padding(.bottom, 3)
-                        .hidden()
-                    Text("F")
+            }
+            .frame(height: 120)
+            HStack(spacing: 8) {
+                ForEach(0..<7) { index in
+                    Text(Date.dayOfTheWeekShortStr(index + 1))
+                        .foregroundColor(
+                            Date().dayOfTheWeek == index + 1 ?
+                                habit.color : ThemeColor.subColor(colorScheme)
+                        )
                         .bodyText()
-                        .frame(width: 24)
-                        .hidden()
+                        .frame(width: 40)
                 }
             }
-            .frame(height: 160)
-            .offset(y: 60)
         }
-        .frame(height: 180)
+    }
+}
+
+struct DayOfWeekChart_Previews: PreviewProvider {
+    static var previews: some View {
+        DayOfWeekChart(habit: DataSample.shared.habitManager.contents[0])
     }
 }
